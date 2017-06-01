b4d148c997ee139271730fd4d767d6517bce1104
Daniel Ochoa John <dochoajohn@gmail.com>
2017-06-01 16:26:03 -0400

    Add more avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,22 @@ class User < ApplicationRecord
   end
 
   def self.create_user(code)
-    create({
+    create(
       code: code,
       avatar: possible_avatars.sample
-    })
+    )
   end
 
   def self.possible_avatars
-    ['https://randomuser.me/api/portraits/thumb/men/83.jpg']
+    [
+      'https://randomuser.me/api/portraits/thumb/men/83.jpg',
+      'https://avatarfiles.alphacoders.com/827/82704.png',
+      'http://vignette3.wikia.nocookie.net/rickandmorty/images/a/a4/Morty-5412.jpg',
+      'https://vignette1.wikia.nocookie.net/phineasandferb/images/8/82/Plat_cap_avatar.png'
+    ] + random_user_avatars
+  end
+
+  def self.random_user_avatars
+    1.upto(90).map { |x| "https://randomuser.me/api/portraits/thumb/men/#{x}.jpg" }
   end
 end
